34cb07cf7a8b462d5cb102a49c303bd604935b31
Матин Манафов <matin.manafovvvv@gmail.com>
2020-11-15 13:29:18 +0400

    UserControl added

---
+++ BakingSystemUI/Controls/RegLogControl.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace BakingSystemUI.Controls
+{
+	partial class RegLogControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.txbx_email = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txbx_password = new System.Windows.Forms.TextBox();
+			this.btn_submit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(22, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Email";
+			// 
+			// txbx_email
+			// 
+			this.txbx_email.Location = new System.Drawing.Point(25, 65);
+			this.txbx_email.Name = "txbx_email";
+			this.txbx_email.Size = new System.Drawing.Size(157, 20);
+			this.txbx_email.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(22, 98);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Password";
+			// 
+			// txbx_password
+			// 
+			this.txbx_password.Location = new System.Drawing.Point(25, 124);
+			this.txbx_password.Name = "txbx_password";
+			this.txbx_password.Size = new System.Drawing.Size(157, 20);
+			this.txbx_password.TabIndex = 4;
+			// 
+			// btn_submit
+			// 
+			this.btn_submit.Location = new System.Drawing.Point(65, 168);
+			this.btn_submit.Name = "btn_submit";
+			this.btn_submit.Size = new System.Drawing.Size(75, 23);
+			this.btn_submit.TabIndex = 6;
+			this.btn_submit.Text = "Submit";
+			this.btn_submit.UseVisualStyleBackColor = true;
+			// 
+			// RegLogControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.btn_submit);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txbx_password);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txbx_email);
+			this.Name = "RegLogControl";
+			this.Size = new System.Drawing.Size(212, 220);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txbx_email;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txbx_password;
+		private System.Windows.Forms.Button btn_submit;
+	}
+}
---
+++ BakingSystemUI/Controls/RegLogControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BakingSystemUI.Controls
+{
+	public partial class RegLogControl : UserControl
+	{
+		public RegLogControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
