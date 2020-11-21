a28e2b4709d50480e086d7cc532f60c5c1244d76
Матин Манафов <matin.manafovvvv@gmail.com>
2020-11-21 13:21:19 +0400

    DataBase Logic added

---
+++ BakingSystemUI/Data/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using BakingSystemUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakingSystemUI.Data
+{
+	public class DbContext
+	{
+		public static DbSet<User> Users;
+		public static DbSet<Card> Cards;
+
+		public DbContext()
+		{
+			Users = new DbSet<User>();
+			Cards = new DbSet<Card>();
+		}
+	}
+}
---
+++ BakingSystemUI/Data/DbSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakingSystemUI.Data
+{
+	public class DbSet<T>
+	{
+		private List<T> _list;
+		public DbSet()
+		{
+			_list = new List<T>();
+		}
+
+		public void Add(T item)
+		{
+			_list.Add(item);
+		}
+
+		public void Remove(T item)
+		{
+			_list.Remove(item);
+		}
+
+		public void Update(T oldItem, T newItem)
+		{
+			_list.Remove(oldItem);
+			_list.Add(newItem);
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			return _list;
+		}
+	}
+}
